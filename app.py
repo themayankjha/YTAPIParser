a885a2773388ad274681e794cbeac25e8f18ba5e
Mayank Jha <jhamayank159@gmail.com>
2021-12-18 12:29:48 +0530

    youtube API implemented

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
-from flask import Flask, render_template, redirect, url_for, request,json
+from flask import Flask, render_template, redirect, url_for, request
 from flask_apscheduler import APScheduler
-from functions import key , makeytrequest, searchdb, writedb
-from keyfile import ytkey
-import requests
+from functions import key , ytreq
 
 app = Flask(__name__)
-yturl='https://www.googleapis.com/youtube/v3/search'
-query='valorant'
 
 @app.route('/', methods=['GET'])
 def home():
@@ -17,13 +13,8 @@ def home():
 def dataout():
     pass
 
-def ytreq(args):
-    output=requests.get(yturl, params={'part': 'snippet','q':query,"key": ytkey})
-    output=json.loads(output.content)
-    writedb(output)
-
 scheduler = APScheduler()
-scheduler.add_job(func=ytreq, args=['args'], trigger='interval', id='job', seconds=10)
+scheduler.add_job(func=ytreq, args=['args'], trigger='interval', id='job', seconds=100)
 scheduler.start()
 
 if __name__ == '__main__':
--- functions.py
+++ functions.py
@@ -1,17 +1,39 @@
+import sqlite3,requests,json
+from datetime import datetime, timedelta, timezone
+from keyfile import ytkey
+
+yturl='https://www.googleapis.com/youtube/v3/search'
+query='valorant'
+
+time=datetime.now(timezone.utc).astimezone() - timedelta(hours=6)  
+mytime = time.isoformat() # get and convert current time to RFC 3339
+
 class key:
     keyworks=True
     def __init__(self,keyid):
         self.keyid=keyid
 
-
-def dbio():
-    pass
-
-def makeytrequest():
-    pass
+def ytreq(args):
+    output=requests.get(yturl, params={'part': 'snippet','q':query,"key": ytkey,'type':'video','order':'date','publishedAfter':mytime})
+    output=json.loads(output.content)
+    writedb(output)
+    print("request made")
 
 def searchdb():
     pass
 
-def writedb(args):
-    print(args)
+def writedb(output):
+    con = sqlite3.connect('static/YTdatabase.db')
+    cur = con.cursor()
+    for vid in output['items']:
+        vidid=vid['id']['videoId']
+        vidpublish=vid['snippet']['publishedAt']
+        vidtitle=vid['snippet']['title']
+        viddesc=vid['snippet']['description']
+        vidthumbnail=vid['snippet']['thumbnails']['default']['url']
+        try:
+            cur.execute('INSERT INTO vidData (title, desc, published,thumbnail,vidid)VALUES (?, ?, ?, ?, ?)', (vidtitle,viddesc,vidpublish,vidthumbnail,vidid))
+        except:
+            pass
+    con.commit()
+    con.close()
